3893c495ee1e396ece6a41380246b6bdfe633bd0
Ayodele Fagbami <afagbami@gmail.com>
2022-06-23 15:08:26 +0100

    done updateTutorial

--- src/main/java/com/example/tutorials/controller/TutorialController.java
+++ src/main/java/com/example/tutorials/controller/TutorialController.java
@@ -53,4 +53,17 @@ public class TutorialController {
             return  new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    @PutMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
+        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+        if (tutorialData.isPresent()) {
+            Tutorial _tutorial = tutorialData.get();
+            _tutorial.setTitle(tutorial.getTitle());
+            _tutorial.setDescription(tutorial.getDescription());
+            _tutorial.setPublished(tutorial.isPublished());
+            return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
