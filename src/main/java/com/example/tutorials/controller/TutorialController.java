0afea7ab17d04751a01350762e1d7b29e41aaf9d
Ayodele Fagbami <afagbami@gmail.com>
2022-06-23 15:09:52 +0100

    done deleteAllTutorials

--- src/main/java/com/example/tutorials/controller/TutorialController.java
+++ src/main/java/com/example/tutorials/controller/TutorialController.java
@@ -75,4 +75,13 @@ public class TutorialController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    @DeleteMapping("/tutorials")
+    public ResponseEntity<HttpStatus> deleteAllTutorials() {
+        try {
+            tutorialRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
